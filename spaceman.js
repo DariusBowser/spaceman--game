1553eb354aef24a2e4ecf38e910620adf29283bf
Sadarius Bowser <sadariusbowser@Sadariuss-MacBook-Air.local>
2023-06-16 15:13:32 -0400

    object

---
+++ spaceman.js
@@ -0,0 +1,19 @@
+// Define Constants
+const objects = [];
+// Define state Variables
+let categories;
+let mistakes;
+let choice;
+let word;
+let guesses;
+let alphabet;
+let hint;
+let guess;
+
+// Cache Elements
+
+// Add Event Listeners
+
+// Invoke the init function
+
+// Invoke the main Render function
