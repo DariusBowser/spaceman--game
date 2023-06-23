6ffe41611ddf04585267ff597b0ca1364b97cbe2
Sadarius Bowser <sadariusbowser@Sadariuss-MacBook-Air.local>
2023-06-22 23:49:04 -0400

    reworked game, worked functions and event listeners out

--- spaceman.js
+++ spaceman.js
@@ -1,115 +1,112 @@
-// window.onload = function() {}
 
-    
     // Define Constants
-    const objects = {
+    const categories = {
         animals: ['Octopus', 'Beaver', 'Iguana', 'Elephant', 'Chimpanzee','Hippopotamus'],
         colors: ['Pink', 'Orange', 'Yellow', 'Purple', 'Burgundy', 'Beige'],
         shapes: ['Rectangle', 'Triangle', 'Sphere', 'Diamond', 'Hexagon', 'Octagon']
     };
-     const lettersContainer = document.getElementsByClassName('letters-container');
-     const topic = document.getElementsByClassName('categories');
-     const playAgain = document.getElementsByClassName('again');
-     const letterGuessElement = document.getElementsByClassName('letter-guess');
+    // HTML components
+    const categoriesDiv = document.getElementById('categories')
+    const letterInputDiv = document.getElementById('letterInput');
+    const letterButtons = document.querySelectorAll('.letter');
+    const categoryButtons = document.querySelectorAll('.category');
+    
+    const letterGuessElement = document.getElementsByClassName('letter-guess');
+
+    //  const playAgain = document.getElementsByClassName('again');
      const chosenWordElement = document.getElementsByClassName('chosen-word');
-     const livesElement = document.getElementsByClassName('lives')
+     const livesElement = document.getElementsByClassName('lives');
 
 
-    // Define state Variables
-    let userInput = document.getElementsByClassName('user-input');
-    let livesLeft = 6;
-    let winCount = 0;
-    let count = 0;
-    let chosenWord = '';
-    let guesses; [];
-    let misses;
-    let choice;
-    let word;
-    let guess;
-    let category;
-    let gameWon; 
-    
-    // Cache Elements
-    
-    
-    // Add Event Listeners
-    const letterButtons = document.getElementsByClassName(`letter`);
-    for (let i = 0; i < letterButtons.length; i++) {
-        letterButtons[i].addEventListener('click', function(evt) {
-            const letter = evt.target.textContent.toLowerCase();
-            processGuess(letter);
-        })
-    }
-    const categoryButtons = document.querySelectorAll('.shapes, .animals, .colors');
-    
-    categoryButtons.forEach(button => {
-        button.addEventListener('click', function(evt) {
-            // Chose category from data
-            const category = evt.target.getAttribute('data-category');
-            // call init from category
-            init(category);
-        });
-    });
+     //variables
+     let userInput = document.getElementsByClassName('user-input');
+     let livesLeft = 6;
+     let chosenWord;
+     let guesses = [];
+     let category;
+     let matches = 0;
+     
+     console.log(categoriesDiv)
+     
+     function chooseWord() {
+         const wordArray = categories[category];
+         const randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+         return randomWord.toLowerCase();
+        }
+        function createBlankLetterInput() {
+            letterInputDiv.innerHTML = ""
+            for (let i = 0; i < chosenWord.length; i++) {
+                const newDiv = `<div id="letterInput${i}">_</div>`
+                letterInputDiv.innerHTML += newDiv
+            }
+        }
+        
+        function checkGameStatus() {
+            if (matches === chosenWord.length) {
+                alert('Congratulations! You won!');
+            }
+            if (livesLeft === 0) {
+                alert('Game Over! The word was '+ chosenWord)
+            }
+        }
 
-    // Functions
-    
-    function chooseWord(category) {
-        const wordArray = objects[category.toLowerCase()];
-        const randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-        return randomWord.toLowerCase();
-    }
-    // Initialize game
-    function init() {
-        chosenWord = chooseWord(category);
-        // guesses initialize
-        guesses = Array(chosenWord.length).fill('_');
-        //update display of chosen words
-        updateChosenWord();
-        updateLivesLeft();
-    }
-    //Function update chosen word
-    function updateChosenWord() {
-        // const chooseWordElement = document.getElementsByClassName('chosen-word');
-        chooseWordElement.textContent = guesses.join(' ');
-    }
-    //Function to update display of lives
-    function updateLivesLeft() {
-        // const livesLeftElement = document.getElementsByClassName('lives');
-        livesLeftElement.textContent = `Lives Remaining: ${livesLeft}`;
-    }
-    
-    function updateLetterGuess(letter) {
-        letterGuessElement.textContent += letter;
-    }
-    // User guess function
-    function processGuess(letter) {
-        if (!gameWon && livesLeft > 0) {
-            const guessedWord = chosenWord.indexOf(letter);
-            if (guessedWord !== -1) {
-                // right guess
-                while (guessedWord !== -1) {
-                    guesses[guessedWord] = letter;
-                    guessedWord = chosenWord.indexOf(letter, guessedWord + 1);
-                }
-                updateChosenWord();
-                if (!guesses.includes('_')) {
-                    //letter guess right
-                    gameWon = true;
-                    alert('Congratulations! You won!');
-                }
-            } else {
-                // wrong guess
-                livesLeft--;
-                updateLivesLeft();
-                // update displayed letter guesses
-                updateLetterGuess(letter);
-                if (livesLeft === 0) {
-                    // out of lives
-                    alert('Game Over! The word was '+ chosenWord);
+        function processGuess(letter) {
+            if (chosenWord.includes(letter)) {
+            for (let i = 0; i < chosenWord.length; i++) {
+                if (chosenWord[i] === letter) {
+                    document.getElementById(`letterInput${i}`).textContent = letter
+                    matches++
                 }
             }
         }
-    }
+        else livesLeft--
+        checkGameStatus()
+        }
+
+        
+        function init() {
+            chosenWord = chooseWord();
+            console.log(chosenWord)
+            // guesses = Array(chosenWord.length).fill('_');
+            // updateChosenWord();
+            // updateLivesLeft();
+            createBlankLetterInput();
+        }
+        // Add Event Listeners
+    categoryButtons.forEach(categoryButton => {
+        categoryButton.addEventListener('click', function(evt) {
+            // Chose category from data
+             category = evt.target.getAttribute('data-category');
+            // call init from category
+            evt.target.disabled = true;
+            init();
+        });
+    });
+    
+    letterButtons.forEach(letterButton => {
+        letterButton.addEventListener('click', function(evt) {
+            const letter = evt.target.textContent.toLowerCase();
+            processGuess(letter);
+            evt.target.disabled = true;
+    })
+        })
+    
+
+    // Functions
+    
+    //Function update chosen word
+    function updateChosenWord() {
+         chosenWordElement.textContent = guesses.join(' ');
+     }
+    //Function to update display of lives
+     function updateLivesLeft() {
+         livesElement.textContent = `Lives Remaining: ${livesLeft}`;
+     }
+     function updateLetterGuess(letter) {
+         letterGuessElement.textContent += letter;
+     }
+
+    // Initialize game
 
     // call init function 
-    // init();
+    //init();
